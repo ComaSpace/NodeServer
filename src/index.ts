3e2b7f1029182f5bcb9698ec3325cf4e38f0004f
ComaSpace <kijucrolandas@gmail.com>
2024-05-29 09:42:28 +0300

    Node

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,131 @@
+import mysql from 'mysql2';
+import { Student } from './models/student';
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
-//Sukuriame f-ją kuri bus paleista tuomet kai atkeliaus užklausa iš vartotojo
-const server=http.createServer((req,res)=>{
+//Kintamasis kuris rodo ar mes prisijungę prie duomenų bazės
+let connected=false;
 
-    //Vartotojo nuoroda
+// Ši dalis sukuria prisijungimą prie duomenų bazės
+const con=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"LabasRytas",
+    database:"students"
+});
+
+//Prisijungia prie duomenų bazės
+// con.connect();
+// con.connect( funkcija kuri bus vykdoma po prisijungimo )
+con.connect((error:any)=>{
+ if (error) throw error;
+
+ //Po prisijungimo be klaidos, nustatome jog esame prisijungę prie DB
+ connected=true;
+
+ console.log("Prisijungta");
+ 
+});
+
+
+
+//Sukuriame http serverį ir paduodame f-ją kuri bus vykdoma kai ateis užklausa
+const server=http.createServer((req, res)=>{
     const url=req.url;
-    console.log(url);
-
-    //Pasiimti metodą
     const method=req.method;
-    console.log(method);
-
-    //Daugiklio paemimas
-    let daugiklis=1;
-    if (url!=null){
-       daugiklis=parseInt(url.split("/")[1]);
-    }
     
 
-    //Išsiunčiame vartotojui kokie duomenys yra persiunčiami
-    res.setHeader('Content-Type','text/html; charset=utf-8');
-    
-    //Siunčiamas dokumentas
-    res.write("<!DOCTYPE html>");
-    res.write("<html>");
-
-    res.write("<head>");
-    res.write("<title>Daugybos lentelė</title>");
-    res.write("</head>");
-    res.write("<body>");
-
-    for(let i=1; i<=10; i++){
-        res.write(`<a href="/${i}">${i}</a>&nbsp;&nbsp;`);
+    /*
+    //Jei kažkas atėjo į puslapį su GET metodu į url: localhost:2999/students, Galime jam išsiųsti JSON formatu duomenis
+    if (url=='/students' && method=='GET'){
+        if (connected){
+            con.query<Student[]>("SELECT * FROM students ORDER BY name ASC;", (error,result)=>{
+                if (error) throw error;
+                res.setHeader("Content-Type", "text/JSON; charset=utf-8");
+                res.write(JSON.stringify(result));        
+                res.end();
+            });   
+        }
     }
-    res.write("<hr>");
-    res.write(`<h1>${daugiklis} daugybos lentelė</h1>`);
-    res.write("<table border='1'>");
-    
-    for (let i=1; i<=10; i++){
-        res.write("<tr>");
-        res.write(`<td>${i}</td><td>*</td><td>${daugiklis}</td><td>=</td><td>${i*daugiklis}</td>`);
-        res.write("</tr>");
-    }
+    */
 
-/*
-    for (let i=1; i<=10; i++){
-        res.write("<tr>");
-        for (let y=1; y<=10; y++){
-            if (i==5 && y==4){
-                res.write("<td></td>");
-            }else{
-                res.write(`<td>${i*y}</td>`);
-            }
+    //Dalis skirta statiniem failam išvesti
+    let filePath=`public${url}`;
+    // fs.existsSync("kelias iki failo")  - patikrina ar failas,katalogas, likas egzistuoja, jei taip, tai garažina 'true', jei ne 'false'
+    // fs.lstatSync(filePath).isFile()  -patikrina ar tai failas (ne katalogas, nuoroda, įrenginys)
+    if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()){
+        const ext=path.extname(filePath);
+        switch (ext) {
+            case ".css":
+                res.setHeader("Content-Type", "text/css; charset=utf-8");
+                break;
+            case ".js":
+                res.setHeader("Content-Type", "application/javascript; charset=utf-8");
+                break;
+            case ".jpg":
+            case ".png":
+            case ".jpeg":
+                res.setHeader("Content-Type", "image/jpg; charset=utf-8");
+                break;
         
         }
-        res.write("</tr>");
+        let file=fs.readFileSync(filePath);
+        res.write(file);
+        return res.end();
     }
-*/
-    res.write("</table>");
 
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
+    //Jei kažkas atėjo į puslapį su GET metodu į url: localhost:2999/students, išsiunčiame jam studentų sąrašą HTML formatu
+    if (url=='/students' && method=='GET'){
+        if (connected){
+            con.query<Student[]>("SELECT * FROM students ORDER BY name ASC;", (error,result)=>{
+                if (error) throw error;
+                res.setHeader("Content-Type", "text/html; charset=utf-8");
+                let rows="";
+                result.forEach((s)=>{ 
+                    rows+="<tr>";
+                    rows+=`<td>${s.name}</td> <td>${s.surname}</td> <td>${s.phone}</td> <td> <a href='/student/${s.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows+="</tr>";
+                });
+               
+                let template=fs.readFileSync('templates/students.html').toString();
+                template=template.replace('{{ students_table }}', rows);
+              
+                res.write(template);        
+                res.end();
+            });   
+        }
+    }
+
+    //Vieno studento atvaizdavimas, kai url = localhost:2999/student/5
+    console.log(url?.split("/"));
+    if ( url?.split("/")[1] == 'student' ){
+        //Pasiimame iš url id
+        let id=parseInt(url?.split("/")[2]);
+        con.query<Student[]>(`SELECT * FROM students WHERE id=${id};`, (error,result)=>{
+            if (error) throw error;
+            let student=result[0];
+            res.setHeader("Content-Type", "text/html; charset=utf-8");
+            
+            let template=fs.readFileSync('templates/student.html').toString();
+            template=template.replace("{{ name }}", student.name);
+            template=template.replace("{{ surname }}", student.surname);
+            template=template.replace("{{ phone }}", student.phone!=null?student.phone:'-');
+            template=template.replace("{{ sex }}", student.sex!=null?student.sex:'-');
+            template=template.replace("{{ email }}", student.email!=null?student.email:'-');
+            template=template.replace("{{ birthday }}", student.birthday!=null?student.birthday.toLocaleDateString():'-');
+            
+          
+            res.write(template);        
+            res.end();
+        });
+        
+        
+    }
+
+    
 
 });
 
-server.listen(2999, 'localhost');
+//Paleidžiame serverį
+server.listen(2999,'localhost');
